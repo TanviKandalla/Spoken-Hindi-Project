061bfe2ebfa07d162f736e5f061f8fe48f2babe0
Tanvi Kandalla <tanvi02kandalla@gmail.com>
2023-09-25 11:03:27 +0530

    Fixed error with written data

--- Dependency Code.py
+++ Dependency Code.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 from io import StringIO
 import pandas as pd
 import os
+import csv
 
 def returnString(text, sentence_id, file_name):
     data = StringIO(text)
-    df = pd.read_csv(data, sep='\t', header=None)
+    df = pd.read_csv(data, sep='\t', header=None, quoting=csv.QUOTE_NONE)
     df = df.fillna(' ')
     string = ''
     for i in range(0, len(df)):
@@ -16,15 +17,14 @@ def returnString(text, sentence_id, file_name):
 
 def modifyFiles(input_folder, output_folder):
     files = os.listdir(input_folder)
-    print(files)
     os.makedirs(output_folder, exist_ok=True)
     for file_name in files:
-        print(file_name)
+        if file_name == "Modified Files":
+            continue
         base_name, file_extension = os.path.splitext(file_name)
         file_path = os.path.join(input_folder, file_name)
         output_file_name = base_name + '_modified.txt'
         output_file_path = os.path.join(output_folder, output_file_name)
-        print(output_file_path)
         with open(file_path, 'r', encoding='utf-8') as file:
             file_contents = file.read()
         
@@ -38,6 +38,7 @@ def modifyFiles(input_folder, output_folder):
         
         with open(output_file_path, 'w', encoding='utf-8') as output_file:
             output_file.write(new_string)
+        print("Done with ", file_path)
 
 def main():
     input_folder = input("Enter the path of the input folder:")
